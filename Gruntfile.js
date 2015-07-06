1de665d1d677fe9321a47c8881b9d383923bc860
eplehans <eplehans@gmail.com>
2015-07-06 16:24:32 +0200

    added copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,20 +6,56 @@ module.exports = function(grunt) {
 
 
 		concat: {
-				options: {
-					//separator: ';'
-				},
-				dist: {
-					src: 'src/js/includes/*.js',
-					dest: 'src/js/main.js'
+			options: {
+				//separator: ';'
+			},
+			dist: {
+				src: 'src/js/includes/*.js',
+				dest: 'dist/js/includes/main.js'
+			}
+		},
+
+		postcss: {
+			options: {
+				map: true, // inline sourcemaps
+
+				processors: [
+					require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+					require('cssnano')() // minify the result
+				]
+			},
+			dist: {
+				src: 'dist/style/css/*.css'
+			}
+		},
+
+		cssnext: {
+			options: {
+				sourcemap: true
+			},
+			dist: {
+				files: {
+					"dist/style/css/nexified.css": "src/style/css/main.css"
 				}
 			}
+		},
 
+		copy: {
+			main: {
+				src: ['src/index.html'],
+				dest: 'dist/'
+			}
+		}
 
 	});
 
 	require("load-grunt-tasks")(grunt);
 
-	grunt.registerTask("watch", "concat");
+	grunt.registerTask("watch", [
+		"concat",
+		"cssnext",
+		"postcss",
+		"copy"
+	]);
 
 };
